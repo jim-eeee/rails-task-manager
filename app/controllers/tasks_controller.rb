d6c5df769ec1e58a3f04900807377f3085343389
James Gallagher <james.joe.gallagher92@gmail.com>
2023-08-15 15:35:15 +0100

    Added link to show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
 end
